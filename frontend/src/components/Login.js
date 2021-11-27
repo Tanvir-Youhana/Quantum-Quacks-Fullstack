be45df477a7dea6120652ac8d41d77aa02492a27
QuocHHDo <qdo000@citymail.cuny.edu>
2021-11-26 19:25:18 -0500

    Connected login/register frontend to backend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,13 +3,27 @@ import { useState, useEffect } from 'react'
 import {Grid, Paper, Avatar, TextField, Button, Typography, Link} from '@mui/material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import './Login.css'
+import axios, { Axios } from "axios"
+import instance from "../axios";
 //  import Logo from './qq.png';
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); 
+
     const paperStyle={padding :20, height: '50vh', width: 280, margin:"150px auto"}
     const avatarStyle={backgroundColor: 'orange'}
     const stylButn ={margin:'8px 0'}
     const stylField ={margin:'8px 0'}
+
+    const login = () => {
+        instance.post("/login", {
+            email: email,
+            password: password 
+        }).then((response) => {
+            console.log(response); 
+        })
+    };
 
     return (
         <div className="login">
@@ -23,9 +37,28 @@ function Login() {
                    </Avatar>
                   <h2>Sign in </h2>
                   </Grid>
-                <TextField label= 'Email' placeholder= 'Enter email...' fullWidth required style= {stylField}/>
-                <TextField label= 'Password' placeholder= 'Enter password...' type= 'password' fullWidth required style= {stylField}/>
-                <Button type='submit' color= "success" variant="contained" halfWidth style={stylButn}>Sign in</Button>
+                <TextField 
+                    label= 'Email' 
+                    placeholder= 'Enter email...' 
+                    onChange= {(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    fullWidth required style= {stylField}/>
+                <TextField 
+                    label= 'Password' 
+                    placeholder= 'Enter password...' 
+                    onChange= {(e) => {
+                        setPassword(e.target.value); 
+                    }}
+                    type= 'password' 
+                    fullWidth required style= {stylField}/>
+                <Button 
+                    type='submit' 
+                    color= "success"
+                    variant="contained" 
+                    onClick= {login}
+                    halfWidth style={stylButn}>Sign in
+                </Button>
                 
                 <Typography > Don't have an Account?  
                 <Link href="./signup" underline="hover">
--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -3,8 +3,24 @@ import { useState, useEffect } from 'react'
 import {Grid, Paper, Avatar, TextField, Button, Typography, Link} from '@mui/material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import './registration.css';
+import instance from "../axios";
 
 function Registration() {
+    const [emailReg, setEmailReg] = useState(""); 
+    const [passwordReg, setPasswordReg] = useState("");
+    const [first_name_Reg, setFirstNameReg] = useState("");
+    const [last_name_Reg, setLastNameReg] = useState(""); 
+
+    const register = () => {
+        instance.post("/register", {
+            email: emailReg,
+            password: passwordReg,
+            first_name: first_name_Reg,
+            last_name: last_name_Reg,
+        }).then((response) => {
+            console.log(response); 
+        })
+    };
         const paperStyle={padding :20, height: '65vh', width: 280, margin:"100px auto"}
         const avatarStyle={backgroundColor: 'orange'}
         const stylButn ={margin:'8px 0'}
@@ -18,11 +34,41 @@ function Registration() {
                    </Avatar>
                   <h2>Create an Account </h2>
                   </Grid>
-                <TextField label= 'First Name' placeholder= 'Enter first name...' fullWidth required style={stylField}/>
-                <TextField label= 'Last Name' placeholder= 'Enter last name...' fullWidth required style={stylField}/>
-                <TextField label= 'Email' placeholder= 'Enter email...' fullWidth required style={stylField}/>
-                <TextField label= 'Password' placeholder= 'Enter password...' type= 'password' fullWidth required style={stylField}/>
-                <Button type='submit' color= "success" variant="contained" halfWidth style={stylButn}>Sign Up</Button>
+                <TextField 
+                    label= 'First Name' 
+                    placeholder= 'Enter first name...' 
+                    onChange= {(e) => {
+                        setFirstNameReg(e.target.value); 
+                    }}
+                    fullWidth required style={stylField}/>
+                <TextField 
+                    label= 'Last Name' 
+                    placeholder= 'Enter last name...' 
+                    onChange= {(e) => {
+                        setLastNameReg(e.target.value); 
+                    }}
+                    fullWidth required style={stylField}/>
+                <TextField 
+                    label= 'Email' 
+                    placeholder= 'Enter email...' 
+                    onChange= {(e) => {
+                        setEmailReg(e.target.value); 
+                    }}
+                    fullWidth required style={stylField}/>
+                <TextField 
+                    label= 'Password' 
+                    placeholder= 'Enter password...' 
+                    onChange= {(e) => {
+                        setPasswordReg(e.target.value); 
+                    }}
+                    type= 'password' 
+                    fullWidth required style={stylField}/>
+                <Button type='submit' 
+                    color= "success" 
+                    variant="contained" 
+                    onClick= {register}
+                    halfWidth style={stylButn}>Sign Up
+                </Button>
                 <Typography > Already have an account?  
                 <Link href="./" underline="hover">
                 {'Sign In'}
