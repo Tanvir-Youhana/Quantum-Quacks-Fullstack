b7d54189290e1eef7a59f3a00f79bfde5fc9fed9
Tanvir-Youhana <Tanvir-Youhana@users.noreply.github.com>
2021-11-27 18:00:09 -0500

    add env variables and database creds

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,15 @@
 import { Sequelize } from "sequelize";
+require("dotenv").config()
 
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_DATABASE = process.env.DB_DATABASE
+const DB_PORT = process.env.DB_PORT
 
 // Need to change credentials 
-const db = new Sequelize('fullstack', 'root', 'root', { // (Database_Name, User, Password)
-    host: "localhost",
+const db = new Sequelize('DB_DATABASE', 'DB_USER', 'DB_PASSWORD', { // (Database_Name, User, Password)
+    host: "DB_HOST",
     dialect: "mysql"
 });
  
