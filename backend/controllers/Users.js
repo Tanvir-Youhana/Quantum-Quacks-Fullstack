05e111b4ae423b7d2df7498a87aaaa15f3282dd8
QuocHHDo <qdo000@citymail.cuny.edu>
2021-12-03 16:49:20 -0500

    Worked on express-session for updatingPassword, etc

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,9 +1,10 @@
 import User from "../models/userModel.js";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+//import bcrypt from 'bcrypt';
+//import jwt from 'jsonwebtoken';
 import _ from 'lodash';
 import db from "../config/database.js";
-const SECRET = "asbadbbdbbh7";
+//const SECRET = "asbadbbdbbh7";
+import session from 'express-session'; 
 
 export const getAllUsers = async (req, res) => {
     try {
@@ -50,7 +51,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
     try 
     {
-        const {email, password, first_name, last_name} = req.body; 
+        const {email, password} = req.body; 
         const user = await User.findOne({where: {email}}).catch(
             (err) => {
                 console.log("Error: ", err);
@@ -61,7 +62,49 @@ export const login = async (req, res) => {
         {
             return res.status(202).json({message: "Email or password does not match!"});
         }
+
+        req.session.email = user.email; 
+        req.session.first_name = user.first_name; 
+        req.session.password = user.password; 
+
+        console.log(req.session.email); 
         res.status(201).json({message: "Login successful!"}); 
+    } catch (e)
+    {
+        res.status(500).send(e.message); 
+    }
+}
+
+export const getLogin = async(req, res) => {
+    if(req.session.email)
+    {
+        res.send({loggedIn: true, email: req.session.email});
+    } else {
+        res.send({loggedIn: false, });
+    }
+}
+export const updatePassword = async (req, res) => {
+    try { 
+        // Check if user did not type in the correct old password
+        if(req.body.old_password != req.session.password)
+        {
+            console.log("Incorrect password!");
+            return res.status(202).json({message: "The old password is incorrect! Please try again."});
+        }
+        
+        // Check if new password does not match confirm password 
+        if(req.body.new_password != req.body.confirm_password)
+        {
+            console.log("New password does not match with confirm password!");
+            return res.status(202).json({message: "The new password does not match with confirm password! Please try again."});
+        }
+        await User.update({ password: req.body.new_password}, {
+            where: {
+                email: req.session.email 
+            }
+        });
+        console.log("Update successful"); 
+
     } catch (e)
     {
         res.status(500).send(e.message); 
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import userRoutes from "./routes/index.js";
 import cors from "cors";
 import User from "./models/userModel.js";
 import bodyParser from "body-parser"; 
+import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const port= process.env.DB_PORT
@@ -21,11 +23,26 @@ db.sync({force: true}).then(() => {
     console.log("Drop and re-sync db.");
   });
 */
-
-app.use(cors());
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true 
+}));
+app.use(cookieParser()); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    key: "emailID",
+    secret: "asbadbbdbbh7",
+    resave: false,
+    saveUninitialize: false,
+    cookie: {
+        expires: 60 * 60 * 24,
+    },
+}));
 //app.use(bodyParser.urlencoded({extended: false}));  
 //app.use(bodyParser.json());
-app.use(express.json());
+
 app.use('/', userRoutes); 
 //app.use('/products', productRoutes);
 
